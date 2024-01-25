9c22d2c7f1b2d5bb5a594bb378d6443a01925a62
atul <atul22gcoders@gmail.com>
2024-01-26 03:43:12 +0530

    Create App

---
+++ src/components/Projects.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity, Linking } from 'react-native'
+import axios from 'axios';
+import { Title, Card, ActivityIndicator, MD2Colors } from 'react-native-paper';
+// css
+import style from '../styles/style'
+
+export default function Projects(props) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Fetch Api Data
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://atugatranapi.pages.dev/' + props.API);
+                setData(response.data);
+            } catch (error) {
+                console.error('asd Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    // On Press
+    const handlePress = (url) => {
+        Linking.openURL(url).catch((err) => console.error('Error opening URL:', err));
+    };
+
+    return (
+        <View style={{ backgroundColor: props.bg, paddingBottom: 15 }}>
+            <Title style={style.projectTitle}>{props.title}</Title>
+            {loading ? (
+                <ActivityIndicator style={style.loading} animating={true} color={MD2Colors.blue800} />
+            ) : (
+                data.map((item, index) => (
+                    <Card key={index} style={style.card}>
+                        <Card.Cover source={{ uri: item.img }} style={{ resizeMode: 'contain' }} />
+                        <Card.Content>
+                            {/* Card Title */}
+                            <Title style={style.cardTitle}>{item.title}</Title>
+                            {/* Links */}
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 10 }}>
+                                <TouchableOpacity onPress={() => handlePress(item.demo)}>
+                                    <Text style={style.cardLink}>Live </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => handlePress(item.src)}>
+                                    <Text style={style.cardLink}>Source Code</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </Card.Content>
+                    </Card>
+                ))
+            )}
+        </View>
+    )
+}
---
+++ src/styles/style.js
@@ -0,0 +1,62 @@
+import { StyleSheet } from 'react-native'
+
+export default style = StyleSheet.create({
+    // Comman
+    spacebottom: {
+        marginBottom: 18
+    },
+    // Heading
+    heading: {
+        fontSize: 40,
+        fontWeight: "600",
+        textAlign: "center",
+        marginTop: 4,
+    },
+    // subHeading
+    subheading: {
+        fontSize: 30,
+        fontWeight: "600",
+        textAlign: "center",
+        marginTop: 4
+    },
+    // Paragrap
+    paragrap: {
+        textAlign: "center",
+        marginTop: 10,
+        fontSize: 16,
+    },
+    // Projects Components
+    // Loading
+    loading: {
+        margin: 15
+    },
+    projectTitle: {
+        textAlign: "center",
+        marginVertical: 14,
+        fontSize: 30,
+        fontWeight: "400"
+    },
+    // Card
+    card: {
+        marginVertical: 8,
+        marginHorizontal: 15
+    },
+    // Card Image
+    cardImg: {
+        width: 600
+    },
+    // Card Title
+    cardTitle:{
+        textAlign: "center",
+        fontSize: 25,
+        fontWeight: "600",
+        marginTop: 15
+    },
+    // Card Link
+    cardLink:{
+        fontSize: 18,
+        marginBottom: 5,
+        fontWeight: "500",
+    }
+})
+
